0a108a797e1a338033ac0e6c965e19c5657c9484
lausandt <laurens.sandt@protonmail.com>
2023-08-04 10:20:21 +0200

    last on enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -20,7 +20,11 @@ fn main() {
     let mut absent_number: Option<i32> = None;  // akin to Maybe Int
     absent_number = Some(55);
 
-    println!("{}", some_number.unwrap() + 7) 
+    println!("{}", some_number.unwrap() + 7);
+
+    println!("{}", value_in_cents(Coin::Penny));
+
+    println!("{}", value_in_cents(Coin::Quarter(UsState::NewYork)));
 
 
 }
@@ -61,14 +65,28 @@ enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("George is a rhino!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state); //debug print
+            25
+        }
     }
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    NewYork,
+    // --snip--
 }
