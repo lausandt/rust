0fa94ed0b1d5cf04f264fcba0a0e9c1f1b39450f
lausandt <laurens.sandt@protonmail.com>
2023-08-11 15:02:08 +0200

    final tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,7 +1,12 @@
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn add(a: i32, b: i32)  -> i32 {
+    internal_adder(a,b)
 }
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 
 pub fn add_two(a: i32) -> i32 {
     a + 2
@@ -32,7 +37,7 @@ impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 {
             panic!(
-                "Guess value must be less than or equal to 100, got {}.",
+                "Guess value must be more than 0 and less than or equal to 100, got {}.",
                 value
             );
         } else if value > 100 {
@@ -45,6 +50,11 @@ impl Guess {
             Guess {value }
         }
     }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
 }
 
 #[cfg(test)]
@@ -91,7 +101,7 @@ mod tests {
 
     #[test]
     fn it_works() -> Result<(), String> {
-        if 2 + 2 == 5 {
+        if 2 + 2 == 4 {
             Ok(())
         } else {
             Err(String::from("two plus two does not equal four"))
@@ -111,7 +121,25 @@ mod tests {
     #[test]
     #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
-        Guess::new(200);
+        Guess::new(-25);
     }
 
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
 }
---
+++ tests/target/debug/deps/integration_test-75cc0b6c5064133d.d
@@ -0,0 +1,6 @@
+C:\Users\anton\OneDrive\Bureaublad\rust\tests\target\debug\deps\integration_test-75cc0b6c5064133d.rmeta: tests\integration_test.rs tests\common\mod.rs
+
+C:\Users\anton\OneDrive\Bureaublad\rust\tests\target\debug\deps\integration_test-75cc0b6c5064133d.d: tests\integration_test.rs tests\common\mod.rs
+
+tests\integration_test.rs:
+tests\common\mod.rs:
