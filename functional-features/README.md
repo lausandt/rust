a570f94854aac460ea80a87e68425f1875bd117e
lausandt <laurens.sandt@protonmail.com>
2023-08-21 12:22:45 +0200

    added closures

--- functional-features/README.md
+++ functional-features/README.md
@@ -23,4 +23,24 @@ assert a(5) == 11
 
 Both return a closure. Like the above Python functions, for Rust the appeal of a closure is to pass the function as an argument and get a function as a result type. Treating function as first class citizens. 
 
+Closures are typically short and relevant only within a narrow context rather than in any arbitrary scenario. Within these limited contexts, the compiler can infer the types of the parameters and the return type. 
+
+Closures can capture values from their environment in three ways, which directly map to the three ways a function can take a parameter: 
+ - borrowing immutably
+ - borrowing mutably
+ - taking ownership
+
+A closure body can do any of the following: 
+ 1. move a captured value out of the closure
+ 2. mutate the captured value
+ 3. neither move nor mutate the value
+ 4. capture nothing from the environment to begin with.
+
+The way a closure captures and handles values from the environment affects which traits the closure implements, and traits are how functions and structs can specify what kinds of closures they can use. Closures will automatically implement one, two, or all three of these Fn traits, in an additive fashion, depending on how the closure’s body handles the values:
+
+- FnOnce applies to closures that can be called once. All closures implement at least this trait, because all closures can be called. A closure that moves captured values out of its body will only implement FnOnce and none of the other Fn traits, because it can only be called once.
+- FnMut applies to closures that don’t move captured values out of their body, but that might mutate the captured values. These closures can be called more than once.
+- Fn applies to closures that don’t move captured values out of their body and that don’t mutate captured values, as well as closures that capture nothing from their environment. These closures can be called more than once without mutating their environment, which is important in cases such as calling a closure multiple times concurrently.
+
+
 
--- functional-features/src/main.rs
+++ functional-features/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 fn main() {
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue, ShirtColor::Green],
@@ -16,6 +18,118 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_pref2, giveaway2
     );
+
+    let one = add_one(5);
+    println!("the first add one is {}", one);
+
+    let two = |num:i32| -> i32 { num + 1};
+    println!("the second add one is an annotated closure {}", two(5));
+
+    let three = |num| { num + 1 };
+    println!("the third add one is a closure with optional syntax {}", three(5));
+
+    let four = |num| num + 1;
+    println!("the fourth add one is a closure with minimal syntax {}", four(5));
+
+    // the compiler infers the type, but once used you can't change types
+    let example_closure = |x| x;
+
+    let s = example_closure(String::from("George is a rhino!"));
+    println!("{}",s);
+
+    //
+    let n = example_closure(String::from("5"));
+    println!("{}",n);
+
+    let f = |_| (); // sometimes called the "toilet closure"
+
+    let s = String::from("Hello");
+
+    println!("{:?}",f(s));
+
+    //immutable borrowing by a closure
+    let list = vec![1,2,3];
+
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = ||  println!("a closure that borrows an immutable {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    //mutable borrowing by a closure
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    //println!("{:?}", list); this will not compile as the list is borrowed but the closure isn't called
+
+    borrows_mutably(); //the mutable borrow ends here
+
+    println!("After calling closure: {:?}", list);
+
+    //taking ownership
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    //move converts any variables captured by reference or mutable reference to variables captured by value
+    // move forces the closure to take ownership of list and therefore preventing it from being dropped 
+    // if the main thread finishes before this thread.
+    thread::spawn(move || println!("From thread: {:?}", list)).join().unwrap();
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:?}", list);
+
+    list.sort_by_key(|r| r.height);
+    println!("{:?}", list);
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    let mut num_sort_operations = 0;
+
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:?} the number of sort operations was {}", list, num_sort_operations);
+
+    //It needs to be explicit that the closure captures the lifetime of s_ref otherwise our function won't compile.
+    fn make_a_cloner<'a>(s_ref: &'a str) -> impl Fn() -> String + 'a{
+        move || s_ref.to_string()
+    }
+
+    let s_own = String::from("George is a rhino!");
+
+    let cloner = make_a_cloner(&s_own);
+
+    //drop(s_own);
+
+    println!("{}", cloner());
+
+    //We can remove the <'a> generic so long as we keep an indicator that the returned closure depends on some lifetime, like this:
+    fn make_a_cloner_simple(s_ref: &str) -> impl Fn() -> String + '_ {
+        move || s_ref.to_string()
+    }
+
+    let mut s = String::from("George");
+
+    let mut add_suffix = |s: &mut String| s.push_str(" is a rhino!");
+
+    println!("{s}");
+
+    add_suffix(&mut s); 
+
+
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -31,7 +145,10 @@ struct Inventory {
 
 impl Inventory {
     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-        user_preference.unwrap_or_else(|| self.most_stocked()) // if the optional value is None return the closure most_stocked()
+        user_preference.unwrap_or_else(|| self.most_stocked()) // || self.most_stocked() is the closure expression 
+                                                               // any argument can be passed between the pipes ||  
+                                                               // The closure captures an immutable reference to the self Inventory instance
+                                                               // and passes it with the code we specify to the unwrap_or_else method
     }
 
     
@@ -64,6 +181,37 @@ impl Inventory {
             }
             
         }
+    }
+
+}
+
+fn add_one(num:i32)->i32{
+    num + 1
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+pub struct Analyzer<F> {
+
+    postprocess: F
+
+}
+
+
+impl<F:Fn(i32) -> i32> Analyzer<F> {
+
+    fn process(&self, n: i32) -> i32 {3}
+
+    pub fn pipeline(&self, n: i32) -> i32 {
+
+        let n = self.process(n);
+
+        (self.postprocess)(n)
+
     }
 
 }
